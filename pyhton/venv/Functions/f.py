c60acba572bb9be9fd4af288914ca55d6e59ce2c
Talg39 <45418888+Talg39@users.noreply.github.com>
2021-06-16 11:57:53 +0300

    Upload test
    Uploaded python project

---
+++ pyhton/venv/Functions/f.py
@@ -0,0 +1,34 @@
+import sys
+import requests
+import validators
+
+
+def is_valid(val):
+    valid = validators.url(val)
+    if valid:
+        return True
+    else:
+        return False
+
+
+def get_url(url):
+    try:
+        results = requests.head(url)
+        return results
+    except:
+        print("===== Something went wrong, we couldn't get url status =====")
+        sys.exit()
+
+
+def decipher_status(statusCode):
+    switcher = {
+        200: "OKK",
+        301: "Permanent Redirect - 301",
+        302: "Temporary Redirect - 302",
+        404: "Not Found - 404",
+        410: "Gone - 410",
+        500: "Internal Server Error - 500",
+        503: "Service Unavailable - 503"
+    }
+    # print(switcher.get(statusCode, "Status code is not recognized as valid"))
+    return switcher.get(statusCode)
---
+++ pyhton/venv/Functions/slack.py
@@ -0,0 +1,19 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+import os
+from slack import WebClient
+from slack.errors import SlackApiError
+
+slack_token = os.environ["SLACK_API_TOKEN"]
+client = WebClient(token=slack_token)
+
+try:
+  response = client.chat_postMessage(
+    channel="C0XXXXXX",
+    text="Hello from your app! :tada:"
+  )
+except SlackApiError as e:
+  # You will get a SlackApiError if "ok" is False
+  assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+
